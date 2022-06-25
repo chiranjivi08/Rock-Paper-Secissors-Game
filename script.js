6991df1a7d4014d156c95e9da350bbdb4c91e21f
Chiranjivi <97388466+chiranjivi08@users.noreply.github.com>
2022-06-25 20:20:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+  userChoice = e.target.id
+  userChoiceDisplay.innerHTML = userChoice
+  generateComputerChoice()
+  getResult()
+}))
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
+  
+  if (randomNumber === 1) {
+    computerChoice = 'rock'
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'scissors'
+  }
+  if (randomNumber === 3) {
+    computerChoice = 'paper'
+  }
+  computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = 'its a draw!🤝'
+  }
+  if (computerChoice === 'rock' && userChoice === "paper") {
+    result = 'you win!✌️'
+  }
+  if (computerChoice === 'rock' && userChoice === "scissors") {
+    result = 'you lost!😭'
+  }
+  if (computerChoice === 'paper' && userChoice === "scissors") {
+    result = 'you win!✌️'
+  }
+  if (computerChoice === 'paper' && userChoice === "rock") {
+    result = 'you lose!😭'
+  }
+  if (computerChoice === 'scissors' && userChoice === "rock") {
+    result = 'you win!✌️'
+  }
+  if (computerChoice === 'scissors' && userChoice === "paper") {
+    result = 'you lose!😭'
+  }
+  resultDisplay.innerHTML = result
+}
